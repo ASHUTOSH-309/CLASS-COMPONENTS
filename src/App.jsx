c6c1940290ee162a9368f97a9511d83d734e63b6
ASHUTOSH-309 <ashutoh.sharma83020@gmail.com>
2024-09-12 22:51:28 +0530

    Counter Program using Class component

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <MyClassComponent />
+
+
+    </>
+  )
+}
+
+
+class MyClassComponent extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+    //this.state will be the object containing the state
+  }
+
+  incrementCount = () => {
+    this.setState({count:this.state.count + 1});
+  }
+
+  render() {
+    return <div>
+      <p>{this.state.count}</p>
+      <button onClick={this.incrementCount}>Increment but from class</button>
+    </div>
+  }
+}
+
+
+
+
+function MyComponent() {
+
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  }
+
+  return (
+    <div>
+      <p>{count}</p>
+      <button onClick={incrementCount}>Click Me</button>
+    </div>
+  )
+}
+
+export default App
